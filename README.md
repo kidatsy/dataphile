546ac5a8b6a482a729ff4abbb38614a99c1e2289
kidat <toshiro.kida@gmail.com>
2015-02-14 14:23:48 -0500

    finally getting set up with swig as template, need to start grunt

--- README.md
+++ README.md
@@ -1,20 +1,42 @@
 # dataphile
 open source code for dataphile.org
 
-NodeJS app, with Nginx proxy, as per here:
+- NodeJS app, with Nginx proxy, as per here:
 https://www.digitalocean.com/community/tutorials/how-to-set-up-a-node-js-application-for-production-on-ubuntu-14-04
 
-Using the following to add to init.d:
+- Using the following to add to init.d:
 https://github.com/chovy/node-startup
 
-.gitignore settings copied from:
+- .gitignore settings copied from:
 https://github.com/github/gitignore/blob/master/Node.gitignore
 
-Getting started with express using:
+- Getting started with express using:
 http://cwbuecheler.com/web/tutorials/2013/node-express-mongo/ and
 http://cwbuecheler.com/web/tutorials/2014/restful-web-app-node-express-mongodb/
 
-You'll need to install dependencies via npm when first starting up:
+- You'll need to install dependencies via npm when first starting up:
 ```
 npm install
 ```
+
+- Also, will need to install mongodb, grunt via npm
+```
+npm install -g grunt-cli
+// cd path/to/app/directory
+// npm install grunt --save-dev
+```
+
+- using the following grunt modules:
+```
+grunt-contrib-sass
+grunt-contrib-handlebars / ember-templates / compile-handlebars // need to play with Ember first before deciding
+grunt-shell
+grunt-contrib-watch
+grunt-contrib-cssmin
+grunt-contrib-nodeunit
+grunt-contrib-yuidoc
+grunt-easy-mongo-fixture
+grunt-express / grunt-express-server // will need to see which works better
+```
+
+- using swig as the template, rather than jade, because it most closely resembles twig
--- app.js
+++ app.js
@@ -5,14 +5,23 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// TUTORIAL - Connect to MongoDB
+var mongo = require('mongodb');         // This sets up the connection to the mongo db
+var monk = require('monk');
+var db = monk('localhost:27017/dataphile');
+
+var cons = require('consolidate');
+var http = require('http');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -22,6 +31,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// TUTORIAL - Make our db accessible to our router
+app.use(function(req,res,next){     // This makes the db accessible by all routes
+    req.db = db;
+    next();
+});
+
 app.use('/', routes);
 app.use('/users', users);
 
@@ -29,10 +44,17 @@ app.use('/users', users);
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
+    console.log('error');
     next(err);
 });
 
 // error handlers
+
+if (app.get('env') === 'development') {
+    app.use(require('express-stackman')());
+}
+
+/*  Default Express error handler. Using stackman for now, above
 
 // development error handler
 // will print stacktrace
@@ -56,5 +78,6 @@ app.use(function(err, req, res, next) {
     });
 });
 
+*/
 
 module.exports = app;
