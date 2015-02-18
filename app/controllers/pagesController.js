2480e4cb42923e3c247c5f256156e4d53b9dc17e
kidat <toshiro.kida@gmail.com>
2015-02-18 08:37:18 -0500

    dumb example, making sure db connection with mongoose works

--- app/controllers/pagesController.js
+++ app/controllers/pagesController.js
@@ -1,10 +1,31 @@
 var locomotive = require('locomotive');
 var Controller = locomotive.Controller;
+var Account = require('../models/account');
 
 var pagesController = new Controller();
 
 pagesController.main = function() {
   this.title = 'Locomotive';
+  this.render();
+}
+
+pagesController.add = function() {
+  this.title = 'Adding user';
+
+    var account = new Account();
+    console.log("shit");
+    account.email = 'asasdf@ase.com';
+    account.name.first = 'werwerwer';
+    account.name.last = 'werwerwervrwaese';
+
+    var self = this;
+    account.save(function (err) {
+    if (err) {
+        console.log('failure!');       
+    }
+        console.log('success!');
+    });
+
   this.render();
 }
 
--- config/routes.js
+++ config/routes.js
@@ -7,4 +7,5 @@
 // information.
 module.exports = function routes() {
   this.root('pages#main');
+  this.match('testingadd', 'pages#add', { via: 'get'});
 }
