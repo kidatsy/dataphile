4f9f399e0741f769c4e765297991dd0e39b9c378
kidat <toshiro.kida@gmail.com>
2015-04-05 13:12:02 -0400

    shit breaking, too long off project. clean swipe

--- config/environments/production.js
+++
@@ -1,3 +0,0 @@
-module.exports = function() {
-    this.set('db-uri', 'mongodb://localhost/dataphile-prod');
-}
--- config/initializers/01_ember.js
+++
@@ -1,7 +0,0 @@
-module.exports = function() {
-
-  require('ember');
-  FrontEnd = Ember.Application.create({
-
-  });
-}
